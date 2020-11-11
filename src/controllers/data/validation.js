9553f901c950df74070094bf9f203e5a9d236991
crispantufla <crispantufla@gmail.com>
2020-11-11 09:04:40 -0800

    Libros y fixes

--- src/controllers/data/validation.js
+++ src/controllers/data/validation.js
@@ -21,7 +21,7 @@ const validationChecks = (req, res, next) => {
   const errors = validationResult(req);
     if(!errors.isEmpty()) {
         let error = {}; errors.array().map((err) => error[err.param] = err.msg);
-        return res.status(500).json({error});
+        return res.status(500).json(error);
     }
 
     next();

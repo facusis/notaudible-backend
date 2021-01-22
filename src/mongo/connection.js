21c99b2573cecc02a0139c2ad04ef1511a08de83
facusis <facusis@gmail.com>
2021-01-22 20:23:02 +0100

    chivato

--- src/mongo/connection.js
+++ src/mongo/connection.js
@@ -17,4 +17,7 @@ if (databaseURL) {
 		mongoose.connect(`mongodb://${databaseHost}:${databasePort}/${databaseName}?authSource=admin`, {useNewUrlParser: true, useUnifiedTopology: true});
 	}
 }
+const db = mongoose.connection;
 
+db.on("error", (error) => console.log(error));
+db.on("open", () => console.log("connected to database"));
